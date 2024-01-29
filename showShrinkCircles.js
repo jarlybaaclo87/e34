b030c6237ec3046cd7d2b21541e8b1c3bb3ef59d
Jarly Love Baaclo <jlbaaclo87@gmail.com>
2024-01-29 23:54:00 +0800

    update

--- showShrinkCircles.js
+++ showShrinkCircles.js
@@ -1,9 +1,9 @@
 window.onload = function(){
     //create a key value pair of colors
     const colors = {
-        'orange':'rgb(232,109,22)',
-        'green':'rgb(15,143,15)',
-        'red':'rgb(249,0,0)'
+        'orange':'rgb(232, 109, 22)',
+        'green':'rgb(15, 143, 15)',
+        'red':'rgb(249, 8, 8)'
     };
     //selecting a default background color
     var bg_color = colors['orange'];
@@ -16,7 +16,7 @@ window.onload = function(){
         //add styles to el
         el.style.position='absolute';
         el.style.left = event.clientX+'px';
-
+        el.style.top = event.clientY+'px';
         el.style.width = size+'px';
         el.style.height = size+'px';
         el.style.backgroundColor = bg_color;
@@ -29,5 +29,30 @@ window.onload = function(){
     //create an eventlistener for click to call the function create
     document.addEventListener('click',create);
     //reload the page
-    // document.getElementById('reset').addEventListener('click', function)
+    document.getElementById('reset').addEventListener('click', function(){
+        window.location.reload();
+    });
+    //process the selected/clicked color and assigned it as background color
+    var button = document.getElementsByClassName('btn');
+    for (let i = 0; i < 3; i++) {
+        button[i].onclick = function(e){
+            e.stopPropagation();
+            highlight(colors[this.innerText]);
+        }
+    }
+    function highlight(color){
+        bg_color = color;
+        //set a highlight using shadow
+        nodes = document.getElementsByClassName('round');
+        for (let i = 0; i < nodes.length; i++) {
+            nodes[i].style.boxShadow='10px 20px 30px black'; 
+        }
+    }
 }
+
+// shrink = function(){
+
+
+
+//     setInterval(shrink, 75);
+// }
